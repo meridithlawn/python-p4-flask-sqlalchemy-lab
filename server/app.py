b35d09ee9d2d740a877aa23068cda6615c0e19c5
Meridith Lawn <meridithlawn2@gmail.com>
2023-06-14 18:34:24 -0500

    tests not passing, all code done

--- server/app.py
+++ server/app.py
@@ -4,13 +4,25 @@ from flask import Flask, make_response
 from flask_migrate import Migrate
 
 from models import db, Zookeeper, Enclosure, Animal
+# from flask import (
+#     Flask,
+#     request,
+#     g,
+#     sessiion,
+#     json,
+#     jsonify,
+#     render_template,
+#     make_response,
+#     url_for,
+#     redirect
+# )
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
 
 migrate = Migrate(app, db)
-
 db.init_app(app)
 
 @app.route('/')
@@ -19,15 +31,62 @@ def home():
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.get(Animal.id == id)
+    # animal = Animal.query.filter(Animal.id == id).first_or_404
+    # animal = Animal.query.filter(Animal.id == id).one_or_none
+    # use filter_by if you want all
+    # animal = Animal.query.filter_by(id == id).first_or_404()
+    # if animal:= db.session.get(Animal, id): most up-to-date version
+    if animal:
+        response_body = f"""
+            <ul> ID: {animal.id} </ul>
+            <ul> Name: {animal.name} </ul>
+            <ul> Species: {animal.species} </ul>
+            <ul> Zookeeper: {animal.zookeeper.name} </ul>
+            <ul> Enclosure: {animal.enclosure.environment} </ul>
+        """
+        return make_response(response_body, 200)
+    else:
+        response_body = f"""
+            <ul> No animal with ID {id}</ul>
+        """
+        return make_response(response_body, 404)
+
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
-
+    if zk:= db.session.get(Zookeeper, id):
+        response_body = f"""
+            <ul> ID: {zk.id}</ul>
+            <ul> Name: {zk.name}</ul>
+            <ul> Birthday: {zk.birthday}</ul>
+        """
+        animal_names = [animal.name for animal in zk.animals]
+        for name in animal_names:
+            response_body += f"<ul>Animal: {name}</ul>"
+        return make_response(response_body, 200)
+    else:
+        response_body = f"""
+            <ul> 404 Not Found any zookeepers with id {id}</ul>
+        """
+        return make_response(response_body, 404)
+    
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
+    if enclosure:= db.session.get(Enclosure, id):
+        response_body = f"""
+        <ul> ID: {enclosure.id},/ul>
+        <ul> Environment: {enclosure.environment}</ul>
+        """
+        enclosure_list = [enclosure.environment for enclosure in enclosure_list]
+        for enclosure in enclosure_list:
+            response_body += f"<ul>Environment: {enclosure}</ul>"
+        return make_response(response_body, 200)
+    else:
+        response_body - f"""
+            <ul> 404 Not Found any environments with id {id},/ul>
+        """
+        return make_response(response_body, 404)
 
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -1,6 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 
+
+from flask_migrate import Migrate
+# from models import db 
+
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 })
@@ -11,13 +15,43 @@ class Zookeeper(db.Model):
     __tablename__ = 'zookeepers'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    birthday = db.Column(db.String, nullable=False)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    animals = db.relationship('Animal', back_populates='zookeeper')
 
 class Enclosure(db.Model):
-    __tablename__ = 'enclosures'
 
+    __tablename__ = 'enclosures'
+    __table_args__= (
+        db.CheckConstraint("environment IN (\'Grass\', \'Sand\', \'Water\')", 
+        name="env_check"),
+    )
     id = db.Column(db.Integer, primary_key=True)
+    environment = db.Column(db.String, nullable=False)
+    open_to_visitors = db.Column(db.Boolean, nullable=False)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    animals = db.relationship('Animal', back_populates='enclosure')
+
 
 class Animal(db.Model):
     __tablename__ = 'animals'
-
+    # __tableargs__= primary key constraints foreign key constraints etc
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True, nullable=False)
+    species = db.Column(db.String, nullable=False)
+
+    zookeeper_id = db.Column(db.Integer, db.ForeignKey("zookeepers.id"))
+    enclosure = db.Column(db.Integer, db.ForeignKey("enclosures.id"), nullable=False)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    # ask question about cascading
+    zookeeper = db.relationship("Zookeeper", back_populates = 'animals')
+    enclosure = db.relationship("Zookeeper", back_populates = 'animals')
+
+def __repr__(self):
+    return f"<Animal #{self.id}:\n"\
+    +f"Name: {self.name}"\
+    +f"Species: {self.species}"
